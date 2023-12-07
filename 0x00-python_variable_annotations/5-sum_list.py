af68787a9e6aa58d82291121a5c591f9ed9f95b3
rodgersxy <rodgersny99@gmail.com>
2023-12-07 13:36:06 +0300

    fix error

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-"""
-type-annotated function sum_list which takes a list input_list of
-floats as argument and returns their sum as a float
-"""
+"""a type-annotated function List"""
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
-    """
-    takes a list input_list of floats as argument
-    returns their sum as a float
-    """
+def sum_list(input_list: List[float]) -> float:
+    """returns sum of the list float"""
     return sum(input_list)
