f35b4722d4d420b6374df4349b81df795102ebfa
rodgersxy <rodgersny99@gmail.com>
2023-12-07 08:56:23 +0000

    annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function that adds two floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    adds two float
+    Args:
+        a: first float
+        b: second float
+    Returns:
+        The sum of two floats
+    """
+    return a + b
