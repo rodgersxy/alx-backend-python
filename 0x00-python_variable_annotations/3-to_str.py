55c569dc346c25dc8375f5ee4caff4588a3c1041
rodgersxy <rodgersny99@gmail.com>
2023-12-07 13:06:45 +0300

    returns string rep

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument
+and return the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float n as argument
+    returns the string representation of the float
+    """
+    return str(n)
